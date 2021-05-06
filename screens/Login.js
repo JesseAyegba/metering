849d14b53851491ae893e66fd4ee3a5ec5dae6a4
Jesse Ayegba <jessejoseph64@gmail.com>
2021-05-06 01:22:22 +0100

    Initial Commit

---
+++ screens/Login.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+function Login() {
+    return(
+        <View>
+
+        </View>
+    )
+}
+
+export default Login;
+
+const styles = StyleSheet.create({
+    login: {
+        
+    }
+})
