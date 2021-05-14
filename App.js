6d3935936e1d5798c9cb95ba194fdc5ecd798887
Jesse Ayegba <jessejoseph64@gmail.com>
2021-05-15 00:20:01 +0100

    Mini deletion

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import "react-native-gesture-handler";
 import React from "react";
-import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import LoginScreen from "./screens/LoginScreen";
