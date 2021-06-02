182d8ad558558d4f4b4b487ecb08d5f0e5126b5c
Jesse Ayegba <jessejoseph64@gmail.com>
2021-06-02 11:31:47 +0100

    Old config added

--- firebase.js
+++ firebase.js
@@ -3,28 +3,25 @@ import "firebase/firestore";
 import "firebase/auth";
 import "firebase/storage";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAu5TTjQQ_wEtDa6RKxwfSPn12Z2p3OlXI",
-  authDomain: "metering-2.firebaseapp.com",
-  projectId: "metering-2",
-  storageBucket: "metering-2.appspot.com",
-  messagingSenderId: "210246229344",
-  appId: "1:210246229344:web:12e5a2c1d7d4358ea65205"
+  apiKey: "AIzaSyB27D8N8faDjCG3wp2MMkROOAG_xfmNfbo",
+  authDomain: "metering-app.firebaseapp.com",
+  projectId: "metering-app",
+  storageBucket: "metering-app.appspot.com",
+  messagingSenderId: "390734720929",
+  appId: "1:390734720929:web:ab07173e61890620e1e5e0",
 };
 
 let app;
 
 if (firebase.apps.length === 0) {
-    app = firebase.initializeApp(firebaseConfig);
+  app = firebase.initializeApp(firebaseConfig);
 } else {
-    app = firebase.app();
+  app = firebase.app();
 }
 
 const db = app.firestore();
 const auth = firebase.auth();
 const storage = app.storage();
 
-export { db, auth, storage }
-
-
+export { db, auth, storage };
